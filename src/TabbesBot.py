cc97d53d127ef3128ebbb8cd54987cde7bc3d44d
RealSnoozemonster <alaaps1@gmail.com>
2017-06-04 11:10:55 +0000

    Initial Database pt.2 END

--- src/TabbesBot.py
+++ src/TabbesBot.py
@@ -10,29 +10,43 @@ class TabbesBot(discord.Client):
     super().__init__(*args, **kwargs)
     self.prefix = kwargs.get('prefix') if (kwargs.get('prefix') != None) else '='
     self.out = status.out()
+    self.default_msg_nolink  = kwargs.get('nolink_msg' ) if (kwargs.get('nolink_msg' ) != None) else 'Links and invites are not allowed in this channel!'
+    self.default_msg_noascii = kwargs.get('noascii_msg') if (kwargs.get('noascii_msg') != None) else 'Ascii and emoji spam are not allowed in this channel'
     self.db_connection = sqlite3.connect('./db/main_database.db')
     self.out.Log("Successfully connected to local database './db/main_database.db' using SQLite3")
     self.db_cursor = self.db_connection.cursor()
   
   def run(self, *args):
-    self.db_cursor.execute('CREATE TABLE IF NOT EXISTS servers(serverid TEXT, servername TEXT)')
+    self.out.Log("Setting up database")
+    self.db_cursor.execute('CREATE TABLE IF NOT EXISTS servers(serverid TEXT, servername TEXT, nolinkmsg TEXT, noasciimsg TEXT)')
+    self.db_connection.commit()
+    self.out.Success("Database initialise complete")
+    self.out.Log("Starting discord client")
     self.loop.run_until_complete(self.start(*args))
+  
+  def close(self, *args):
+    self.db_cursor.close()
+    self.db_connection.close()
   
   @asyncio.coroutine
   def on_ready(self):
-    self.out.Success("Client Ready! Logged in as " + self.user.id)
+    self.out.Success("Client ready! Logged in as " + self.user.id)
   
   @asyncio.coroutine
-  def on_server_join(server):
-    pass
+  def on_server_join(self, server):
+    self.db_cursor.execute('INSERT INTO servers (serverid, servername, nolinkmsg, noasciimsg) VALUES(?, ?, ?, ?)',
+                           (server.id, server.name, self.default_msg_nolink, self.default_msg_noascii))
+    self.db_connection.commit()
   
   @asyncio.coroutine
-  def on_server_update(before, after):
-    pass
+  def on_server_update(self, before, after):
+    self.db_cursor.execute("UPDATE servers SET servername = '" + after.name + "'WHERE serverid = '" + after.id + "'")
+    self.db_connection.commit()
   
   @asyncio.coroutine
-  def on_server_remove(server):
-    pass
+  def on_server_remove(self, server):
+    self.db_cursor.execute("DELETE FROM servers WHERE serverid = '" + server.id + "'")
+    self.db_connection.commit()
   
   @asyncio.coroutine
   def on_message(self, message):
